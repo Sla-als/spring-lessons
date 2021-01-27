7336a8960b8eeee511f0af03c14486d8dcad4e94
Vyaceslav Sv <nr5wqdncrvuq@mail.ru>
2021-01-27 10:40:08 +0300

    added Service layer, add productsFind method

--- src/main/java/ru/geekbrains/spring/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/spring/controllers/ProductController.java
@@ -1,10 +1,14 @@
 package ru.geekbrains.spring.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.spring.model.Product;
 import ru.geekbrains.spring.reposisitory.ProductRepository;
+import ru.geekbrains.spring.servce.ProductService;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -20,28 +24,21 @@ public class ProductController {
 //        удаление товара по id.[ GET .../app/products/delete/{id} ] +
 //        (Замечание: пока делаем немного не по правилам REST API, эта тема будет разбираться на следующих занятиях, поэтому удаление выполняется через http-метод GET, а не DELETE)
 
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable Long id) {
-        return productRepository.findById(id).get();
-    }
-
-    @GetMapping
-    public List<Product> findAll() {
-        return productRepository.findAll();
+        return productService.getProductById(id);
     }
 
     @GetMapping("delete/{id}")
-    public List<Product> deleteById(@PathVariable Long id) {
-        productRepository.deleteById(id);
-        return productRepository.findAll();
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
     }
 
     @PostMapping  // для создания новых ресурсов
     public Product save(@RequestBody Product product) {
-        //product.setId(null);
-        return productRepository.save(product);
+        return productService.save(product);
     }
 
 //          * К запросу всех товаров добавьте возможность фильтрации по минимальной и максимальной цене
@@ -50,57 +47,18 @@ public class ProductController {
 //            товары дешевле max цены, +
 //            или товары, цена которых находится в пределах min-max). +
 
-    // Методы к заданию со * :
-//    @GetMapping("/test")
-//    public List<Product> test(@RequestParam(name = "min") Integer min) {
-//        return productRepository.findAllByPriceGreaterThanEqual(min);
-//    }
-//    @GetMapping("/test")
-//    public List<Product> test(@RequestParam(name = "min") Integer min,@RequestParam(name = "max") Integer max) {
-//        return productRepository.findAllByPriceBetween(min,max);
-//    }
-//    @GetMapping("/test")
-//    public List<Product> test(@RequestParam(name = "max") Integer max) {
-//        return productRepository.findAllByPriceLessThanEqual(max);
-//    }
-
-
+    @GetMapping
+    public List<Product> productsFind(
+            @RequestParam(name = "min", required = false) Integer min,
+            @RequestParam(name = "max", required = false) Integer max
+    ) {
+        if (max != null && min != null) {
+            return productService.between(min,max);
+        }else if (max != null){
+            return productService.less(max);
+        }else if (min != null){
+          return productService.greater(min);
+        }
+        return productService.findAll();
+    }
 }
-
-
-//
-//@RestController
-//@RequestMapping("/students")
-//public class StudentController {
-//
-//    private final StudentRepository studentRepository;
-//
-//    @GetMapping
-//    public List<Student> getAllStudents(){
-//        return (List<Student>) studentRepository.findAll();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public Student getStudentById(@PathVariable Long id){
-//        return studentRepository.findById(id).get();
-//    }
-//
-//    @GetMapping("/search_by_name")
-//    public Student searchByName(@RequestParam String name) {
-//        return studentRepository.findByName(name).get();
-//    }
-//
-//    @GetMapping("/search_by_min_score")
-//    public List<Student> searchByMinScore(@RequestParam (name = "min_score") Integer minScore) {
-//        return studentRepository.findAllByScoreIsGreaterThanEqual(minScore);
-//    }
-//    @GetMapping("/test")
-//    public Student testMethod() {
-//        return studentRepository.customFinderById(2L, "Bob").get();
-//    }
-//
-//    public StudentController(StudentRepository studentRepository) {
-//        this.studentRepository = studentRepository;
-//    }
-//
-//}
