dc6c0434ae9dc29c3df227bcc8c437f1716ffb81
Vyaceslav Sv <nr5wqdncrvuq@mail.ru>
2021-01-31 13:53:37 +0300

    lesson 09 created commit

--- src/main/java/ru/geekbrains/happy/market/HappyMarketApplication.java
+++ src/main/java/ru/geekbrains/happy/market/HappyMarketApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HappyMarketApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HappyMarketApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(HappyMarketApplication.class, args);
+	}
 }
--- src/main/java/ru/geekbrains/happy/market/services/ProductService.java
+++ src/main/java/ru/geekbrains/happy/market/services/ProductService.java
@@ -1,35 +1,56 @@
 package ru.geekbrains.happy.market.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import ru.geekbrains.happy.market.dto.ProductDto;
 import ru.geekbrains.happy.market.model.Product;
 import ru.geekbrains.happy.market.repositories.ProductRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public Optional<Product> findProductById(Long id) {
-        return productRepository.findById(id);
+
+    // +
+    public Optional<ProductDto> findProductById(Long id) {
+        return productRepository.findById(id).stream().map(ProductDto::new).findFirst();
+    }
+//    public Optional<Product> findProductById(Long id) {
+//        return productRepository.findById(id);
+//    }
+
+    public List<ProductDto> findAll() {
+        return productRepository.findAll().stream().map(ProductDto::new).collect(Collectors.toList());
     }
 
-    public List<Product> findAll() {
-        return productRepository.findAll();
+    public Page<ProductDto> findAll(int page) {
+        Page<Product> originalPage = productRepository.findAll(PageRequest.of(page - 1, 5));
+        return new PageImpl<>(originalPage.getContent().stream().map(ProductDto::new).collect(Collectors.toList()), originalPage.getPageable(), originalPage.getTotalElements());
     }
 
-    public List<Product> findAllByPrice(int min, int max) {
-        return productRepository.findAllByPriceBetween(min, max);
+    // +
+    public List<ProductDto> findAllByPrice(int min, int max) {
+        return productRepository.findAllByPriceBetween(min, max).stream().map(ProductDto::new).collect(Collectors.toList());
     }
+//    public List<Product> findAllByPrice(int min, int max) {
+//        return productRepository.findAllByPriceBetween(min, max);
+//    }
 
+    // -
     public Product saveOrUpdate(Product product) {
         return productRepository.save(product);
     }
 
-    public void deleteProductById(Long id) {
+    // -
+    public void deleteById(Long id) {
         productRepository.deleteById(id);
     }
 }
